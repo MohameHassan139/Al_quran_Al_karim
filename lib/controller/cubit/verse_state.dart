3a1bdcc0bc948201a1a21b557771173be39ef129
Mohamed Hassan <126608001+MohameHassan139@users.noreply.github.com>
2024-06-30 02:38:27 +0300

    fix slider stream

--- lib/controller/cubit/verse_state.dart
+++ lib/controller/cubit/verse_state.dart
@@ -1,3 +1,54 @@
+// part of 'verse_cubit.dart';
+
+// @immutable
+// abstract class VerseState {}
+
+// class VerseInitial extends VerseState {}
+
+// class AduioPlaying extends VerseState {}
+
+// class AudioPlayerLoaded extends VerseState {
+//   final Duration position;
+//   final Duration bufferedPosition;
+//   final Duration duration;
+//   final double volume;
+//   final double speed;
+//   final bool playing;
+
+//   AudioPlayerLoaded({
+//     required this.position,
+//     required this.bufferedPosition,
+//     required this.duration,
+//     required this.volume,
+//     required this.speed,
+//     required this.playing,
+//   });
+
+//   AudioPlayerLoaded copyWith({
+//     Duration? position,
+//     Duration? bufferedPosition,
+//     Duration? duration,
+//     double? volume,
+//     double? speed,
+//     bool? playing,
+//   }) {
+//     return AudioPlayerLoaded(
+//       position: position ?? this.position,
+//       bufferedPosition: bufferedPosition ?? this.bufferedPosition,
+//       duration: duration ?? this.duration,
+//       volume: volume ?? this.volume,
+//       speed: speed ?? this.speed,
+//       playing: playing ?? this.playing,
+//     );
+//   }
+// }
+
+// class AudioPlayerError extends VerseState {
+//   final String message;
+
+//   AudioPlayerError(this.message);
+// }
+
 part of 'verse_cubit.dart';
 
 @immutable
@@ -8,16 +59,14 @@ class VerseInitial extends VerseState {}
 class AduioPlaying extends VerseState {}
 
 class AudioPlayerLoaded extends VerseState {
-  final Duration position;
-  final Duration bufferedPosition;
+  final Stream<PositionData> positionDataStream;
   final Duration duration;
   final double volume;
   final double speed;
   final bool playing;
 
   AudioPlayerLoaded({
-    required this.position,
-    required this.bufferedPosition,
+    required this.positionDataStream,
     required this.duration,
     required this.volume,
     required this.speed,
@@ -25,16 +74,14 @@ class AudioPlayerLoaded extends VerseState {
   });
 
   AudioPlayerLoaded copyWith({
-    Duration? position,
-    Duration? bufferedPosition,
+    Stream<PositionData>? positionDataStream,
     Duration? duration,
     double? volume,
     double? speed,
     bool? playing,
   }) {
     return AudioPlayerLoaded(
-      position: position ?? this.position,
-      bufferedPosition: bufferedPosition ?? this.bufferedPosition,
+      positionDataStream: positionDataStream ?? this.positionDataStream,
       duration: duration ?? this.duration,
       volume: volume ?? this.volume,
       speed: speed ?? this.speed,
---
+++ lib/test.dart
@@ -0,0 +1,19 @@
+// import 'dart:async';
+
+// funStream() {
+//   StreamSubscription<int>? subscription;
+
+//   subscription =
+//       Stream.periodic(const Duration(seconds: 1), (i) => i).listen((i) {
+//     if (i > 5) {
+//       subscription?.cancel();
+//     } else {
+//       print(i);
+//     }
+//   });
+// }
+
+// void main() {
+//   print("Hello World");
+//   funStream();
+// }
